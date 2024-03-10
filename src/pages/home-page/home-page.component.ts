73208f13993c0ab0e9b30e2d33a1633455e58d86
Stepan Berkunov <studiorain27@gmail.com>
2024-03-10 18:24:14 +0300

    feat: add alignment

--- src/pages/home-page/home-page.component.ts
+++ src/pages/home-page/home-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'owl-home-page',
   standalone: true,
-    imports: [
-        NgForOf,
-        TranslateModule
-    ],
+  imports: [
+    NgForOf,
+    TranslateModule,
+    ButtonModule
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })

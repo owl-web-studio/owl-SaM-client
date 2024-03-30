fdf0310b988a79561e1bc7c6bcc683133fef14b2
Stepan Berkunov <studiorain27@gmail.com>
2024-03-30 19:19:38 +0300

    feat: add organization-info page

--- src/pages/organization-page/organization-page.component.ts
+++ src/pages/organization-page/organization-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'owl-organization-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    ButtonModule
+  ],
   templateUrl: './organization-page.component.html',
   styleUrl: './organization-page.component.scss'
 })

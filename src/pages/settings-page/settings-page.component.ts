1b71de8c329115a4da09d79d426e7af05ed72934
berkunov <55670335+Haze272@users.noreply.github.com>
2024-04-18 17:49:15 +0300

    feat: add backup chapter

--- src/pages/settings-page/settings-page.component.ts
+++ src/pages/settings-page/settings-page.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import {AccountService} from "../../services/account.service";
 import {NgForOf} from "@angular/common";
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {DividerModule} from "primeng/divider";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'owl-settings-page',
   standalone: true,
   imports: [
     NgForOf,
-    TranslateModule
+    TranslateModule,
+    DividerModule,
+    ButtonModule
   ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss'

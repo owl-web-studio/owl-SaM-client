a11ecbd4fb274a208748899a179bd2fec44e56af
Stepan Berkunov <studiorain27@gmail.com>
2024-05-16 22:52:27 +0300

    feat: add roles page

---
+++ src/pages/roles/roles-page/roles-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'owl-roles-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles-page.component.html',
+  styleUrl: './roles-page.component.scss'
+})
+export class RolesPageComponent {
+
+}

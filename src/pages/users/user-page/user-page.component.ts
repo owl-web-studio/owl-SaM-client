3236d2ad1eaab71eb07247287f75313cc593fcdc
Stepan Berkunov <studiorain27@gmail.com>
2024-05-22 02:00:00 +0300

    feat: add user card

--- src/pages/users/user-page/user-page.component.ts
+++ src/pages/users/user-page/user-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "../../../services/user.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {Subject, switchMap, takeUntil} from "rxjs";
+import {User} from "../../../entities/user.model";
 
 @Component({
   selector: 'owl-user-page',
@@ -7,6 +11,31 @@ import { Component } from '@angular/core';
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss'
 })
-export class UserPageComponent {
+export class UserPageComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  user: User | undefined;
 
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly userService: UserService
+  ) {
+    this.activatedRoute.paramMap
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap((params: ParamMap) => {
+          return this.userService.getUserInfo(Number(params.get('id')));
+        }),
+      )
+      .subscribe(user => {
+        this.user = user;
+      })
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
